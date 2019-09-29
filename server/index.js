c0205089c96af32b05f807ee5989b4bdaa9adba0
lielgrott <lielgrott@gmail.com>
2019-09-29 15:55:13 +0300

    integrate user router to express app

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT || 3000, 10);
 
-app.post('/', (req, res) => {
-    let name = req.query.name || 'foo';
-    res.send(`hello ${name}`);
-});
+const userRouter = require('./users/users.router');
+
+app.use('/users', userRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
