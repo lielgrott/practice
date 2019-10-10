5dc6de8fc16059806a0293edf5c905e6fed610c9
lielgrott <lielgrott@gmail.com>
2019-10-10 13:13:08 +0300

    middleware

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = parseInt(process.env.PORT || 3000, 10);
 
 const userRouter = require('./users/users.router');
+const postRouter = require('./posts/posts.router');
+
+app.use(express.json());
 
 app.use('/users', userRouter);
+//app.use('/posts', postRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
